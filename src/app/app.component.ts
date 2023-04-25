4361a861253afbde2933601911c94a41495e354e
Ricardo Ramirez <6ricardo6ramirez6@gmail.com>
2023-04-25 18:50:18 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from 'src/app/button/button.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+  
+  results = "Resultado";
+
+  a:number = 0;
+  b:number = 0;
+  c:number = 0;
+  sign:number=0
+
+  
+
+  suma(a:number, b:number){
+    this.c = (a+b);
+    return (this.c)
+  }
+
+  setAB(num:number,){
+    if (this.a == 0) 
+    {this.a = num} else
+    {this.b = num}
+  }
+
+  setSign(num:number){
+  /*  usar do while para setear el signo en "sign" a partir de distintos valores. Ej: 1 == suma / 1 == operaciónDeSuma(a,b) */
+  }
+
 }
