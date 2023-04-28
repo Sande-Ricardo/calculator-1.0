b8d20ff650ea36530ae9b982bd649b774e898fc3
Ricardo Ramirez <6ricardo6ramirez6@gmail.com>
2023-04-28 18:55:54 -0300

    etapa básica (con errores y falta de funciones)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,28 +9,91 @@ import { ButtonComponent } from 'src/app/button/button.component';
 export class AppComponent {
   title = 'Calculator';
   
-  results = "Resultado";
 
   a:number = 0;
   b:number = 0;
+  collect:string="";
   c:number = 0;
-  sign:number=0
+  d:string = "";
+  sign:number=0;
 
   
+  
 
-  suma(a:number, b:number){
-    this.c = (a+b);
-    return (this.c)
-  }
+  sum(){
+    this.c = (this.a+this.b);
+    this.d="+";
+  };
+  
+  rest(){
+    this.c= (this.a-this.b);
+    this.d="-";
+  };
 
-  setAB(num:number,){
-    if (this.a == 0) 
-    {this.a = num} else
-    {this.b = num}
-  }
+  prod(){
+    this.c=(this.a*this.b);
+    this.d="*";
+  };
+
+  coc(){
+    this.c=(this.a/this.b);
+    this.d="/";
+  };
+
+  ac(){
+    this.a=0;
+    this.b=0;
+    this.sign=0;
+    this.collect="";
+  };
+
+  setAB(num:number){
+    this.collect += (num.toString());
+  };
+
+  setP(){
+    if (Number(this.collect)!= 0)
+      {this.collect += (".")}
+    else {alert("Error")};
+    /* arreglar: el alert error debería salir si se pone un punto luego de otro punto*/
+  };
 
   setSign(num:number){
-  /*  usar do while para setear el signo en "sign" a partir de distintos valores. Ej: 1 == suma / 1 == operaciónDeSuma(a,b) */
+    this.sign=num;
+    this.a=Number(this.collect);
+    this.collect="";
+    /* console.log(this.a) */
+  };
+
+  setOp(){
+  /*  usar do while/ switch para setear el signo en "sign" a partir de distintos valores. Ej: 1 == suma / 1 == operaciónDeSuma(a,b) */
+    switch (this.sign){
+      case 1:
+        this.sum();
+        break;
+      case 2:
+        this.rest();
+        break;
+      case 3:
+        this.prod();
+        break;
+      case 4:
+        this.coc();
+        break;
+      
+    }
+
+  };
+
+  igualdad(){
+    this.b=Number(this.collect)
+    this.setOp();
+    console.log(this.c)
+    this.ac()
   }
+
+ 
+  /*creo que no hace falta setear algún numero, solo debería asociar un número para cada operación. 
+  Ej: fijo un 1 en la suma y el event hace que se setee el 1 en sign, el igual corre la función respecto al valor de sign*/
 
 }
